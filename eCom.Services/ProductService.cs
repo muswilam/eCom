f979319484c1b51282e2739208648d32e39949e1
Sully <jackswilam@gmail.com>
2019-09-25 22:23:59 +0200

    Search operation using ajax & build Product CRUD

---
+++ eCom.Services/ProductService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using eCom.Entities;
+using eCom.Data;
+
+namespace eCom.Services
+{
+    public class ProductService
+    {
+        public Product GetProduct(int id)
+        {
+            using (var context = new eComContext())
+            {
+                return context.Products.Where(c => c.Id == id).FirstOrDefault();
+            }
+        }
+
+        public List<Product> GetProducts()
+        {
+            using (var context = new eComContext())
+            {
+                return context.Products.ToList();
+            }
+        }
+
+        public void SaveProduct(Product product)
+        {
+            using (var context = new eComContext())
+            {
+                context.Products.Add(product);
+                context.SaveChanges();
+            }
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            using (var context = new eComContext())
+            {
+                context.Entry(product).State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+        public void DeleteProduct(int id)
+        {
+            using (var context = new eComContext())
+            {
+                var product = context.Products.Find(id);
+
+                context.Products.Remove(product);
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ eCom.Web/Controllers/ProductController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using eCom.Services;
+using eCom.Entities;
+
+namespace eCom.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductService PServices = new ProductService();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult ProductTable(string search)
+        {
+            var products = PServices.GetProducts();
+
+            if(!string.IsNullOrEmpty(search))
+                products = products.Where(p => p.Name.ToLower().Contains(search.ToLower())).ToList();
+            
+            return PartialView(products);
+        }
+
+        [HttpGet]
+        public PartialViewResult Create()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            PServices.SaveProduct(product);
+
+            return RedirectToAction("ProductTable");
+        }
+    }
+}
